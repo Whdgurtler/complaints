29895b19f2ad58d395f805d91816c9edc0353c5d
William Gurtler <whdgurtler@gmail.com>
2025-11-23 14:48:49 -0600

    Add new file:   AB_Test.ipynb

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,5 @@
 import time
 import pandas as pd
-default to one year ago
 from datetime import date, timedelta
 def preprocess_data(first_date_to_keep = date.today() - timedelta(days=365), companies_to_keep: list = None):
     
@@ -30,7 +29,7 @@ def preprocess_data(first_date_to_keep = date.today() - timedelta(days=365), com
     dta.columns = dta.columns.str.lower().str.replace(' ', '_')
     
     #remove rows with missing consumer complaint narrative
-    dta = dta.dropna(subset=['Consumer complaint narrative'])
+    dta = dta.dropna(subset=['consumer_complaint_narrative'])
 
     #convert date received to datetime
     dta['date_received'] = pd.to_datetime(dta['date_received'])
@@ -38,11 +37,16 @@ def preprocess_data(first_date_to_keep = date.today() - timedelta(days=365), com
     dta['date_received'] = dta['date_received'].dt.date
 
     #filter to only include rows with date_received on or after first_date_to_keep
+    #make first_date_to_keep a date object instead of string
+    if isinstance(first_date_to_keep, str):
+        first_date_to_keep = pd.to_datetime(first_date_to_keep).date()
     dta = dta[dta['date_received'] >= first_date_to_keep]
 
     dta['narr_length'] = dta.consumer_complaint_narrative.str.len()
     #drop narrative gt 4930 characters
     dta = dta.loc[dta.narr_length <= 4930, :]
+    #keep only specified companies
+    dta = dta[dta.company.isin(companies_to_keep)]
    
 
     #reset index
